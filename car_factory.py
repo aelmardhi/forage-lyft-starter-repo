b2d8474d9ed9e2723f33a8110eaaa6c61ac7b509
aelmardhi <aelmardhi@gmail.com>
2023-02-08 16:47:45 +0200

    remove unued

--- car_factory.py
+++ car_factory.py
@@ -8,29 +8,31 @@ from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
 
 class CarFactory():
-    def __init__(self) -> None:
-        pass
-
+    @staticmethod
     def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = CapuletEngine( current_mileage, last_service_mileage)
         return Car(engine, battery)
     
+    @staticmethod
     def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int)-> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = WilloughbyEngine( current_mileage, last_service_mileage)
         return Car(engine, battery)
     
+    @staticmethod
     def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_on: bool) -> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = SternmanEngine( warning_light_on)
         return Car(engine, battery)
     
+    @staticmethod
     def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         battery = NubbinBattery(current_date, last_service_date)
         engine = WilloughbyEngine( current_mileage, last_service_mileage)
         return Car(engine, battery)
     
+    @staticmethod
     def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         battery = NubbinBattery(current_date, last_service_date)
         engine = CapuletEngine( current_mileage, last_service_mileage)
