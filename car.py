011076901478320a97b3e718b0c4c8ba721f978a
aelmardhi <aelmardhi@gmail.com>
2023-02-09 11:58:41 +0200

    task 4 . 2

--- car.py
+++ car.py
@@ -3,14 +3,17 @@ from abc import ABC
 from Serviceable import Serviceable
 from battery.battery import Battery
 from engine.engine import Engine
+from tire.tire import Tire
 
 class Car(Serviceable,ABC):
-    def __init__(self, engine: Engine, battery: Battery) -> None:
+    def __init__(self, engine: Engine, battery: Battery, tire: Tire) -> None:
         super().__init__()
         self.engine = engine
         self.battery = battery
+        self.tire = tire
+
 
     def needs_service(self) -> bool:
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service()
         
     
--- car_factory.py
+++ car_factory.py
@@ -1,39 +1,48 @@
 
 from datetime import datetime
+from array import array
+
 from car import Car
 from battery.nubbin_battery import NubbinBattery
 from battery.spindler_battery import SpindlerBattery
 from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
+from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 
 class CarFactory():
     @staticmethod
-    def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
+    def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int, tire_sensor_readings: array) -> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = CapuletEngine( current_mileage, last_service_mileage)
-        return Car(engine, battery)
+        tire = CarriganTire(tire_sensor_readings)
+        return Car(engine, battery, tire)
     
     @staticmethod
-    def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int)-> Car:
+    def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int, tire_sensor_readings: array)-> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = WilloughbyEngine( current_mileage, last_service_mileage)
-        return Car(engine, battery)
+        tire = CarriganTire(tire_sensor_readings)
+        return Car(engine, battery, tire)
     
     @staticmethod
-    def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_on: bool) -> Car:
+    def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_on: bool, tire_sensor_readings: array) -> Car:
         battery = SpindlerBattery(current_date, last_service_date)
         engine = SternmanEngine( warning_light_on)
-        return Car(engine, battery)
+        tire = CarriganTire(tire_sensor_readings)
+        return Car(engine, battery, tire)
     
     @staticmethod
-    def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
+    def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int, tire_sensor_readings: array) -> Car:
         battery = NubbinBattery(current_date, last_service_date)
         engine = WilloughbyEngine( current_mileage, last_service_mileage)
-        return Car(engine, battery)
+        tire = OctoprimeTire(tire_sensor_readings)
+        return Car(engine, battery, tire)
     
     @staticmethod
-    def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
+    def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int, tire_sensor_readings: array) -> Car:
         battery = NubbinBattery(current_date, last_service_date)
         engine = CapuletEngine( current_mileage, last_service_mileage)
-        return Car(engine, battery)
+        tire = CarriganTire(tire_sensor_readings)
+        return Car(engine, battery, tire)
